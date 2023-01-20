26cb17c9f0ba3ec479655972450810d9cb19d280
Emre <emrekurtt1655@gmail.com>
2023-01-20 20:35:29 +0100

    added test for create a todo

--- client/cypress/fixtures/mockData.js
+++ client/cypress/fixtures/mockData.js
@@ -8,3 +8,9 @@ export const loginData = {
   email: "test1@test.com",
   password: "test1234",
 };
+
+
+export const todoData = {
+  text: "Wash the dishes" + new Date().getMilliseconds(),
+  todoDone: "todo",
+};
---
+++ client/cypress/integration/todo.spec.js
@@ -0,0 +1,34 @@
+import { todoData, loginData } from "../fixtures/mockData";
+
+describe("Todo funtions Test Suite", function () {
+  before("It logins to the api", function () {
+    cy.request("POST", Cypress.env("url_Backend") + "login", {
+      email: loginData.email,
+      password: loginData.password,
+    }).then((response) => {
+      cy.checkPostApiMessage(response.body, "Login successfully completed!");
+      expect(response.body).to.have.property("access_token");
+      this.token = response.body["access_token"];
+      this.userId = response.body["user"].userId;
+      this.userName = response.body["user"].userName;
+    });
+  });
+
+  it("todo test funtions it should login", function () {
+    cy.visit(Cypress.env("url_Frontend") + "login");
+    cy.fillForm("email", loginData.email);
+    cy.fillForm("password", loginData.password);
+    cy.get(".loginForm__loginButton").click();
+    cy.get(".toast-header").then((text) => {
+      const successMsg = text.text();
+      expect(successMsg.includes("Success")).to.be.true;
+    });
+    cy.visit(Cypress.env("url_Frontend"));
+    cy.get("#text");
+    cy.fillForm("text", todoData.text).type("{enter}");
+    cy.get(".toast-header").then((text) => {
+      const successMsg = text.text();
+      expect(successMsg.includes("Success")).to.be.true;
+    });
+  });
+});
